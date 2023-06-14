67b18d2fec28c4388b4a2d2104193c2521ee81e7
DavidBicalho <david.lucasb@hotmail.com>
2023-06-14 19:57:22 -0300

    carrossel_inicio

---
+++ script.js
@@ -0,0 +1,8 @@
+var Bruna = window.document.getElementById("Bruna")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+
+function RolarParaDireita() {
+    Leonardo.style ="display:none"
+    Bruna.style ="display:flex"
+}
