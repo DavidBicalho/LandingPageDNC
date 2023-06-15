da429233d343fc5cc6936dd39a2707d4e9ee6169
DavidBicalho <david.lucasb@hotmail.com>
2023-06-15 20:39:39 -0300

    Form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var setaDireita = window.document.getElementById("setadireita")
+var setaEsquerda = window.document.getElementById("setaesquerda")
 var Bruna = window.document.getElementById("Bruna")
 var Leonardo = window.document.getElementById("Leonardo")
 var Samantha = window.document.getElementById("Samantha")
@@ -5,4 +7,15 @@ var Samantha = window.document.getElementById("Samantha")
 function RolarParaDireita() {
     Leonardo.style ="display:none"
     Bruna.style ="display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex"
+
+}
+
+function RolarParaEsquerda() {
+    Leonardo.style = "display:flex"
+    Bruna.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
+
 }
